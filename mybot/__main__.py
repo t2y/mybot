f7270494816f104a033dcb707e50876179190d14
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2016-07-18 11:14:49 +0900

    fix to set weather settings

--- mybot/__main__.py
+++ mybot/__main__.py
@@ -3,10 +3,14 @@ import os
 import sys
 sys.path.append(os.path.dirname(__file__))
 
-from slackbot.bot import Bot  # noqa
-
 
 def main():
+    from slackbot import settings
+    from slackbot.bot import Bot
+
+    from .plugins.weather import OpenWeatherMap
+    settings.open_weather_map = OpenWeatherMap()
+
     bot = Bot()
     bot.run()
 
